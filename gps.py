1594d4e7ee97b30ec507edfb20d1ead9d965f42c
admin <entrance.gate.1508@gmail.com>
2024-09-20 17:43:41 +0900

    Update 9/20

--- gps.py
+++ gps.py
@@ -1,29 +1,36 @@
 # -*- coding:utf-8 -*-
-# location はGPSデータを取得する標準モジュールです
 import ui, location, csv, datetime, time
 
+# GPSデータを日付＋実データとして保存し、CSVに書き込む
 
-# NEWボタンをタップすると呼ばれるメソッド
-def shinki(sender):
-    label = sender.superview["label1"]
+
+def GetInfo():
+    location.start_updates()  # GPSデータ更新を開始
+    gps = location.get_location()  # GPSデータを取得する
+    location.stop_updates()  # GPSデータ更新を終了
+
     now = datetime.datetime.now().strftime("%Y-%m-%d %H:%M:%S")  # 現時刻を取得
-
-    # 1.GPSデータを取得
-    location.start_updates()  # GPSデータ更新を開始
-    gps = location.get_location()  # GPSデータを取得する
-    location.stop_updates()  # GPSデータ更新を終了
-
-    # 2.GPSデータを画面に表示
     gps_text = ""
     for g in gps:
-        gps_text = gps_text + str(g) + ":" + str(gps[g]) + "\n"
-    label.text = now + "\n" + gps_text  # UIのテキストビューにGPSデータを代入
+        gps_text += str(g) + ":" + str(gps[g]) + "\n"  # GPSデータをテキストに変換
+    return gps, gps_text, now
 
-    # 3-1.GPSデータを編集して、csvファイルに保存
-    gps_dict = {"time": now}  # GPSデータはディクショナリ型。
-    gps_dict.update(gps)  # 先に取得時間を入れ、次にGPSデータを追加
 
-    # 3-2.csvファイル用にヘッダー(ディクショナリ型)を作成
+# NEWボタンをタップすると呼ばれるメソッド
+def New(sender):
+    label = sender.superview["label1"]
+
+    # GetInfo からデータを取得
+    gps, gps_text, now = GetInfo()
+
+    # 2.GPSデータを画面に表示
+    label.text = now + "\n" + gps_text  # UIのテキストビューにGPSデータを表示
+
+    # 3-1.GPSデータを編集して、csvファイルに保存
+    gps_dict = {"time": now + ","}  # GPSデータはディクショナリ型
+    gps_dict.update(gps)  # 取得時間とGPSデータをまとめる
+
+    # 3-2.csvファイル用にヘッダー(ディクショナリ型)を作成
     gps_rows = []  # 空のリストを作成
     gps_rows.append(gps_dict)
     parameters = [
@@ -37,13 +44,26 @@ def shinki(sender):
         "speed",
         "course",
     ]
-    header = dict([(val, val) for val in parameters])
-
-    # csvファイルを開いて、gps辞書を書き込むshinki_button
-    with open("gps_log.csv", mode="w", encoding="utf-8") as f:
+    # csvファイルを開いて、gps辞書を書き込む
+    with open("gps_log.csv", mode="a", encoding="utf-8") as f:
         writer = csv.DictWriter(f, parameters, extrasaction="ignore")
         writer.writerows(gps_rows)
 
 
+# 新たにGPSデータを取得し、既存のデータに追加して保存
+def Add(sender):
+    label = sender.superview["label1"]
+    gps, gps_text, now = GetInfo()
+
+
+# 保存していたCSVファイルを初期化
+def Clear(sender):
+    with open("gps_log.csv", mode="w", encoding="utf-8") as f:
+        f.write("")
+    # ラベルに初期化したことを表示
+    sender.superview["label1"].text = "初期化しました"
+
+
+# UIの読み込みと表示
 v = ui.load_view("UI.pyui")
 v.present("sheet")
