4f26443d742fb819be070d59c888bf186b7ca697
admin <entrance.gate.1508@gmail.com>
2024-09-21 23:30:16 +0900

    引数のデータを文字分割をする際に、データそれぞれに対して、引数の処理を柔軟に設定できるUnionの実装

---
+++ Union.py
@@ -0,0 +1,27 @@
+# Description: This file demonstrates the use of Union in Python.
+#   Union is used to specify the type of the input parameter.
+#   In this example, the input parameter can be either a string, int, or float.
+
+# Reference: careful to use Union
+# https://qiita.com/sergicalsix/items/6480902f0f0e0d5bf782
+
+from typing import Union
+
+
+def PrintChar(txt: Union[str, int, float]) -> None:
+    if isinstance(txt, int):
+        print("Converted int to string")
+        txt = str(txt)
+    elif isinstance(txt, float):
+        print("Converted float to string")
+        txt = str(txt)
+    for character in txt:
+        print(character, end="\n")
+
+
+if __name__ == "__main__":
+    PrintChar(341)
+    print("\n")
+    PrintChar("Hello")
+    print("\n")
+    PrintChar(3.13)
